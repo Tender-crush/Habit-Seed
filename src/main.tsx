8c5474e7df7037747bbcd8021eee010b07cc3b84
Tender-crush <15231320813@163.com>
2026-03-13 19:38:14 +0800

    更改路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'  // 导入 BrowserRouter
+import { HashRouter } from 'react-router-dom'  // 导入 HashRouter
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter>      {/* 在这里包裹 Router */}
+        <HashRouter>      {/* 在这里包裹 Router */}
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Habit-Seed/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    sourcemap: true, // 生产环境也生成 source map
   }
 })
